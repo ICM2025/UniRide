764663f631c192738a5c3f62dd308608e8705729
Julian Ramos <julianc.ramos21@gmail.com>
2025-03-15 16:16:21 -0500

    feat: add provisional driver manage trips activity for menu navigation testing

---
+++ app/src/main/java/com/example/uniride/DriverManageTripsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.uniride
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class DriverManageTripsActivity : BottomMenuActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val contentView = layoutInflater.inflate(R.layout.activity_driver_manage_trips, binding.container, false)
+        binding.container.removeAllViews()
+        binding.container.addView(contentView)
+
+        binding.bottomNav.selectedItemId = R.id.nav_manage
+    }
+}
