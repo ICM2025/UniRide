74959cddd96404246beb10681b87683aab879073
SergioOrtiz145 <sergiodoh0308@gmail.com>
2025-03-17 12:46:49 -0500

    feat: add transitions of register activity

--- app/src/main/java/com/example/uniride/RegisterActivity.kt
+++ app/src/main/java/com/example/uniride/RegisterActivity.kt
@@ -1,20 +1,24 @@
 package com.example.uniride
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uniride.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.ingresarButton.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.createAccountButton.setOnClickListener{
+            startActivity(Intent(this, BottomMenuActivity::class.java))
         }
     }
 }
