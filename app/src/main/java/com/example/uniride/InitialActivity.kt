17526e1ab3b2f50c97107933143f90adc3e86244
SergioOrtiz145 <sergiodoh0308@gmail.com>
2025-03-17 12:23:26 -0500

    feat: add transitions of initial activity

--- app/src/main/java/com/example/uniride/InitialActivity.kt
+++ app/src/main/java/com/example/uniride/InitialActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uniride
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -10,16 +11,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uniride.databinding.ActivityInitialBinding
 
 class InitialActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInitialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_initial)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityInitialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.login.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.register.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
 
     }
