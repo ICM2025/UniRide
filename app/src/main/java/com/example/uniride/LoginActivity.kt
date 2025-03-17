71bbd17aea498f12696b975cc0e0e1f5347b0c5c
SergioOrtiz145 <sergiodoh0308@gmail.com>
2025-03-17 12:36:55 -0500

    feat: add transitions of login activity

--- app/src/main/java/com/example/uniride/LoginActivity.kt
+++ app/src/main/java/com/example/uniride/LoginActivity.kt
@@ -1,20 +1,27 @@
 package com.example.uniride
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uniride.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.registerButton.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+        binding.ingresarButton.setOnClickListener{
+            startActivity(Intent(this, BottomMenuActivity::class.java))
+        }
+        binding.olvidoText.setOnClickListener{
+            startActivity(Intent(this, RecoverPasswordActivity::class.java))
         }
     }
 }
