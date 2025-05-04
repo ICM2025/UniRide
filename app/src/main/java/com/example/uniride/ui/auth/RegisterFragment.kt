171dd618b22fb0c1a49f4ea743ea538e681f38c7
SergioOrtiz145 <sergiodoh0308@gmail.com>
2025-05-04 16:02:46 -0500

    feat: add user registration with Supabase

--- app/src/main/java/com/example/uniride/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/uniride/ui/auth/RegisterFragment.kt
@@ -2,14 +2,22 @@ package com.example.uniride.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.uniride.R
+import com.example.uniride.connection.SupabaseInstance
 import com.example.uniride.databinding.FragmentRegisterBinding
 import com.example.uniride.ui.MainActivity
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
 class RegisterFragment : Fragment() {
 
@@ -32,9 +40,40 @@ class RegisterFragment : Fragment() {
         }
 
         binding.createAccountButton.setOnClickListener {
-            val intent = Intent(requireContext(), MainActivity::class.java)
-            startActivity(intent)
-            requireActivity().finish()
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            val username = binding.user.text.toString()
+
+            var valid = true
+            //se revisa si se han completado todos los campos
+            if (email.isEmpty()) {
+                binding.email.error = "El correo es obligatorio"
+                valid = false
+            } else {
+                binding.email.error = null
+            }
+
+            if (password.isEmpty()) {
+                binding.password.error = "La contraseña es obligatoria"
+                valid = false
+            } else {
+                binding.password.error = null
+            }
+
+            if(username.isEmpty()){
+                binding.user.error = "El usuario es obligatorio"
+                valid = false
+            } else{
+                binding.user.error = null
+            }
+            //en el caso de que no, se envia un mensaje de error
+            if (!valid)
+                return@setOnClickListener
+
+            // Corrutina para ejecutar la función de login
+            lifecycleScope.launch {
+                registerUser(email, username, password)
+            }
         }
 
     }
@@ -43,4 +82,29 @@ class RegisterFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+    //Una función de tipo suspend es una función que puede ser pausada y
+    //reanudada sin bloquear el hilo en el que se ejecuta. Permitiendo
+    //escrbir código asíncrono, cosa necesaria para utilizar supabase
+
+    suspend fun registerUser(email1: String, username: String, password1: String) {
+        try {
+            //se crea el usuario con su correo, contraseña y se agrega su nombre de usuario
+             SupabaseInstance.client.auth.signUpWith(Email) {
+                email = email1
+                password = password1
+                data = buildJsonObject {
+                    put("username", username)
+                }
+            }
+            Log.i("Register", "Se registro correctamente")
+            //Se dirige a la actividad home de la app
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+
+        } catch (e: Exception) {
+            println("Error al registrar: ${e.localizedMessage}")
+        }
+    }
+
 }
